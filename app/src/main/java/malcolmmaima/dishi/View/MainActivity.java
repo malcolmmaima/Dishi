488ce5304eee88c9fe57a6f1f0dacc7f35b9fb0e
malcolmmaima <malcolmmaima@gmail.com>
2018-05-24 15:44:05 +0300

    Welcome screens

--- app/src/main/java/malcolmmaima/dishi/View/MainActivity.java
+++ app/src/main/java/malcolmmaima/dishi/View/MainActivity.java
@@ -1,7 +1,9 @@
-package malcolmmaima.dishi;
+package malcolmmaima.dishi.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import malcolmmaima.dishi.R;
 
 public class MainActivity extends AppCompatActivity {
 
