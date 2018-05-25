3a67b489e167e4ba7fe7ef3357e012dd5e43bd57
malcolmmaima <malcolmmaima@gmail.com>
2018-05-25 15:01:53 +0300

    Phone verification with firebase

--- app/src/main/java/malcolmmaima/dishi/View/WelcomeActivity.java
+++ app/src/main/java/malcolmmaima/dishi/View/WelcomeActivity.java
@@ -1,16 +1,13 @@
 package malcolmmaima.dishi.View;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import malcolmmaima.dishi.Controller.PreferenceManager;
-import malcolmmaima.dishi.R;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +17,9 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import malcolmmaima.dishi.Controller.PreferenceManager;
+import malcolmmaima.dishi.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
