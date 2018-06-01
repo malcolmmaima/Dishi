1d1c3175aa16bd59ca1384493b1899f90b9e8937
malcolmmaima <malcolmmaima@gmail.com>
2018-06-01 22:23:26 +0300

    Add menu Item Toolbar and menu

--- app/src/main/java/malcolmmaima/dishi/View/AddMenu.java
+++ app/src/main/java/malcolmmaima/dishi/View/AddMenu.java
@@ -1,7 +1,16 @@
 package malcolmmaima.dishi.View;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import malcolmmaima.dishi.R;
 
@@ -11,5 +20,63 @@ public class AddMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_menu_activity);
+
+        Toolbar topToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        setTitle("Add New Item");
+
+        //Back button on toolbar
+        topToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent slideactivity = new Intent(getApplicationContext(), MyAccount.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                Bundle bndlanimation = ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation,R.anim.animation2).toBundle();
+                startActivity(slideactivity, bndlanimation);
+            }
+        });
+        //topToolBar.setLogo(R.drawable.logo);
+        //topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_new){
+            //Toast.makeText(MyAccount.this, "Add Menu", Toast.LENGTH_LONG).show();
+
+            Intent slideactivity = new Intent(AddMenu.this, AddMenu.class);
+            Bundle bndlanimation =
+                    ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.animation,R.anim.animation2).toBundle();
+            startActivity(slideactivity, bndlanimation);
+
+        }
+        if (id == R.id.action_multiple) {
+            Toast.makeText(AddMenu.this, "Add Multiple", Toast.LENGTH_LONG).show();
+        }
+        if(id == R.id.action_save){
+            Toast.makeText(AddMenu.this, "Save Menu", Toast.LENGTH_LONG).show();
+        }
+        if(id == R.id.action_save2){
+            Toast.makeText(AddMenu.this, "Save Menu", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
