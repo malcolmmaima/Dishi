94d598b79f1f5f1b3601383269560e096ac696e1
malcolmmaima <malcolmmaima@gmail.com>
2018-06-02 16:48:50 +0300

    minor layout xml changes

--- app/src/main/java/malcolmmaima/dishi/View/Fragments/ItemOneFragment.java
+++ app/src/main/java/malcolmmaima/dishi/View/Fragments/ItemOneFragment.java
@@ -22,6 +22,6 @@ public class ItemOneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_item_one, container, false);
+        return inflater.inflate(R.layout.fragment_restaurant_orders, container, false);
     }
 }
